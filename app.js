fabe572e54ba5a986acac2072ebc225793831102
Elio Acosta <eliojavier86@gmail.com>
2018-04-02 00:28:17 -0400

    ajustes

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 app.use('/', index);
 app.use('/accounts', accounts);
 app.use('/admin', admin);
